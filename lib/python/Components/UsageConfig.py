149c93fc9e0bcc1a46eab6367e074d46e0237846
Koivo <koivo@live.de>
2015-07-30 15:37:04 +0200

    [usage] change remote text support to true

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -12,7 +12,7 @@ from Tools.HardwareInfo import HardwareInfo
 
 def InitUsageConfig():
 	config.misc.useNTPminutes = ConfigSelection(default = "30", choices = [("30", "30" + " " +_("minutes")), ("60", _("Hour")), ("1440", _("Once per day"))])
-	config.misc.remotecontrol_text_support = ConfigYesNo(default = False)
+	config.misc.remotecontrol_text_support = ConfigYesNo(default = True)
 
 	config.workaround = ConfigSubsection()
 	config.workaround.deeprecord = ConfigYesNo(default = False)
